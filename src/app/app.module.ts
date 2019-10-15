e5820d3df2a2d6679518c79619e4bf3fcdd6a7b5
Gabriel Batista <svbgabriel@gmail.com>
2019-10-14 21:03:48 -0300

    Implementa o painel de oportunidades

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { AppComponent } from './app.component';
+import { TableModule } from "primeng/table";
+import { PanelModule } from "primeng/panel";
+import { InputTextModule } from "primeng/inputtext";
+import { ButtonModule } from "primeng/button";
+import { ToastModule } from "primeng/toast";
+import { MessageService } from "primeng/api";
+
+import { AppComponent } from "./app.component";
+import { PainelNegociacaoComponent } from "./painel-negociacao/painel-negociacao.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, PainelNegociacaoComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    FormsModule,
+    HttpClientModule,
+    InputTextModule,
+    PanelModule,
+    TableModule,
+    ToastModule
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
